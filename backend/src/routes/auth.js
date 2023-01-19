155533cd85c2b5f1420bb081b7998bd1279c6b2f
unknown <mustafahaider351@gmail.com>
2023-01-19 20:03:34 +0300

    haha

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,23 +1,26 @@
-const express = require('express');
-const authController = require('../controllers/auth');
-const verifyJWT = require('../middleware/VerifyJwt')
-const router = express.Router()
-const {validateSignUp, validateSignIn, validateResult} = require(`../middleware/validator`)
+const express = require("express");
+const authController = require("../controllers/auth");
+const verifyJWT = require("../middleware/VerifyJwt");
+const router = express.Router();
+const {
+  validateSignUp,
+  validateSignIn,
+  validateResult,
+} = require(`../middleware/validator`);
 
-router.route("/sign-up").post(validateSignUp, validateResult, authController.postSignUp)
-router.route("/sign-in").post(validateSignIn, validateResult, authController.postSignIn)
+router
+  .route("/sign-up")
+  .post(validateSignUp, validateResult, authController.postSignUp);
+router
+  .route("/sign-in")
+  .post(validateSignIn, validateResult, authController.postSignIn);
 
+router.route("/secret").post(verifyJWT, (req, res) => {
+  console.log(req.role);
+  console.log(req.apiToken);
+  console.log("it wokred");
+  console.log(req.bob);
+  res.send("I am a backend developer aaaah");
+});
 
-
-
-
-router.route("/secret").post(verifyJWT, (req, res)=> {
-    console.log(req.role);
-    console.log(req.apiToken);
-    console.log("it wokred");
-    console.log(req.bob);
-    res.send("hi")
-})
-
-
-module.exports = router
+module.exports = router;
