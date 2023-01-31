3e466846db67a77de4b8a165f523af867fb076de
unknown <mustafahaider351@gmail.com>
2023-01-31 23:27:37 +0300

    deleted a file

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,59 +1,51 @@
+const bcryptjs = require("bcryptjs");
+const mongoose = require("mongoose");
 
-const bcryptjs = require('bcryptjs');
-const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const Schema = mongoose.Schema
-
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        index: true,
-        trim: true ,
-        unique: true,
-        lowercase:true
+      type: String,
+      required: true,
+      index: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
     },
     password: {
-        type: String,
-        min: 8
+      type: String,
+      min: 8,
     },
     email: {
-        type: String,
-        required: true,
-        trim: true ,
-        unique: true,
-        lowercase:true
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
     },
     role: {
-        type: String,
-        enum: [`user`, `admin`],
-        default: `user`
+      type: String,
+      enum: [`user`, `admin`],
+      default: `user`,
     },
-    contactNumber: {type: String},
-    profilePicture: {type: String}
+    contactNumber: { type: String },
+    profilePicture: { type: String },
+  },
+  { timestamps: true }
+);
 
-}, {timestamps: true})
-
-
-
-
-UserSchema.pre("save", async function  (req, res, next) {
-        const hashed_Password =   await bcryptjs.hash(this.password, 10)
-        this.password =  hashed_Password
-})
-
-
-
-
+UserSchema.pre("save", async function (req, res, next) {
+  const hashed_Password = await bcryptjs.hash(this.password, 10);
+  this.password = hashed_Password;
+});
 
 UserSchema.methods = {
-    authenticate:  function(password){
-        return bcryptjs.compare(password, this.hash_Password)
-    } 
-}
+  authenticate: function (password) {
+    return bcryptjs.compare(password, this.hash_Password);
+  },
+};
 
-const UserModel = mongoose.model("User", UserSchema)
+const UserModel = mongoose.model("User", UserSchema);
 
-
-
-module.exports =   UserModel
+module.exports = UserModel;
