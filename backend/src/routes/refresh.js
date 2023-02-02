97e09317dd11815078cfd151ed7d757ad99318cc
Raython <rasheed998855@gmail.com>
2023-02-02 07:44:51 +0300

    gg

--- backend/src/routes/refresh.js
+++ backend/src/routes/refresh.js
@@ -3,19 +3,19 @@ const authController = require("../controllers/auth");
 const verifyJWT = require("../middleware/VerifyJwt");
 const router = express.Router();
 const cookieParser = require("cookie-Parser");
-const JWT = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const UserModel = require("../models/User");
 
 router.use(cookieParser());
 router.route("/refresh").get((req, res, next) => {
-  console.log("it worked");
-  const jwt = req.cookies.jwt;
-  if (!jwt) return res.status(401).json({ message: "Unauthorized" });
-  JWT.verify(jwt, process.env.JWT_REFRESH_TOKEN_KEY, async (err, decode) => {
-    if (err) return res.status(403).json({ message: "Unauthorized" });
+  const jwtcookie = req.cookies.jwt;
+  if (!jwtcookie) return res.status(401).json({ message: "Unauthorized" });
+  jwt.verify(jwtcookie, process.env.JWT_REFRESH_TOKEN_KEY, async (err, decode) => {
+    if (err) return res.status(403).json({ message: err });
     const foundUser = await UserModel.findOne({
       username: decode.Userinfo.username,
     });
+    console.log("worked before user");
     if (!foundUser) return res.status(401).json({ message: `Unauthorized ` });
     // this line shoube be moved
     const accessToken = jwt.sign(
