e1bc752bf2db9e0453465973801ba1a0ef07f4f7
Raython <rasheed998855@gmail.com>
2023-03-31 00:40:44 +0300

    s

--- backend/src/controllers/cart.js
+++ backend/src/controllers/cart.js
@@ -5,64 +5,70 @@ const ObjectId = require("mongoose").Types.ObjectId;
 
 module.exports.addToCart = async (req, res, next) => {
   const { cartItems, price } = req.body;
+  console.log();
   try {
-    const existCart = await cartModel.findOne({ userId: req.id }).exec();
-    if (existCart) {
-      const isProductExsist = existCart.cartItems.find(
-        (e) => e.product == cartItems.product
-      );
-      if (isProductExsist) {
-        const doc = await cartModel.updateOne(
-          {
-            userId: req.id,
-            "cartItems.product": cartItems.product,
-          },
-          {
-            $inc: {
-              "cartItems.$.quantity": 1,
-            },
-          }
+    if (req.body.length > 0) {
+      const existCart = await cartModel.findOne({ userId: req.id }).exec();
+      if (existCart) {
+        const isProductExsist = existCart.cartItems.find(
+          (e) => e.product == cartItems.product
         );
-        return res.status(200).json({ message: "quantity added " });
-      } else {
-        const isValidObjectId = ObjectId.isValid(cartItems.product);
-        if (isValidObjectId) {
-          const isProductId = await productModel.findOne({
-            _id: cartItems.product,
-          });
-          if (isProductId) {
-            cartModel.updateOne(
-              { userId: req.id },
-              {
-                $push: {
-                  cartItems: {
-                    product: cartItems.product,
-                    price: price,
+        if (isProductExsist) {
+          const doc = await cartModel.updateOne(
+            {
+              userId: req.id,
+              "cartItems.product": cartItems.product,
+            },
+            {
+              $inc: {
+                "cartItems.$.quantity": 1,
+              },
+            }
+          );
+          return res.status(200).json({ message: "quantity added " });
+        } else {
+          const isValidObjectId = ObjectId.isValid(cartItems.product);
+          if (isValidObjectId) {
+            const isProductId = await productModel.findOne({
+              _id: cartItems.product,
+            });
+            if (isProductId) {
+              cartModel.updateOne(
+                { userId: req.id },
+                {
+                  $push: {
+                    cartItems: {
+                      product: cartItems.product,
+                      price: price,
+                    },
                   },
-                },
-              }
-            );
-            existCart.cartItems.push({ cartItems });
-            await existCart.save();
-            return res.status(201).json({ existCart });
+                }
+              );
+              existCart.cartItems.push({ cartItems });
+              await existCart.save();
+              return res.status(201).json({ existCart });
+            } else {
+              res.status(400).json({ message: " no such product" });
+            }
           } else {
             res.status(400).json({ message: " no such product" });
           }
-        } else {
-          res.status(400).json({ message: " no such product" });
         }
+      } else {
+        const addCartToUser = new cartModel({
+          userId: req.id,
+          cartItems: cartItems,
+        });
+        addCartToUser.save((err, result) => {
+          if (err) return res.status(400).json({ message: `error ${err}` });
+          if (result) return res.status(201).json({ message: result });
+        });
       }
     } else {
-      const addCartToUser = new cartModel({
-        userId: req.id,
-        cartItems: cartItems,
-      });
-      addCartToUser.save((err, result) => {
-        if (err) return res.status(400).json({ message: `error ${err}` });
-        if (result) return res.status(201).json({ message: result });
-      });
+      return res.status(204).json({ message: "there is no item to add" });
     }
   } catch (error) {
+    console.log(error);
     return res.status(400).json({ message: "error happened" });
   }
 };
@@ -79,18 +85,18 @@ exports.getCartItems = async (req, res) => {
     .findOne({ user: req.id })
     .populate("cartItems.product", "_id name price productPictures")
     .exec((error, cart) => {
-      console.log("it worked 2?");
+      console.log(error);
       if (error) return res.status(400).json({ error });
       if (cart) {
         let cartItems = {};
-        cart.cartItems.forEach((item, index) => {
+        cart.cartItems.forEach((item) => {
           console.log(`product id  = ${item.product._id}`);
           console.log(`product name  = ${item.product.name}`);
           console.log(`product pic  = ${item.product.productPictures[0].img}`);
           console.log(`product id  = ${item.product.price}`);
           console.log(`product qty  = ${item.quantity}`);
-          cartItems[item.product._id.toString()] = {
-            _id: item.product._id.toString(),
+          cartItems[item.product._id] = {
+            _id: item.product._id,
             name: item.product.name,
             img: item.product.productPictures[0].img,
             price: item.product.price,
