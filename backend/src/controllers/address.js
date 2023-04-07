4df2da97d96ed7afec7199576b3ad63a01699da3
Raython <rasheed998855@gmail.com>
2023-04-07 07:55:33 +0300

    ss

--- backend/src/controllers/address.js
+++ backend/src/controllers/address.js
@@ -1,50 +1,54 @@
-const address = require("../models/adress");
-const UserAddress = require("../models/User");
+const address = require("../models/address")
+const UserAddress = require("../models/address")
 
 exports.addAddress = (req, res) => {
   //return res.status(200).json({body: req.body})
-  const { payload } = req.body;
-  if (payload.address) {
-    if (payload.address._id) {
-      UserAddress.findOneAndUpdate(
-        { user: req.id, "address._id": payload.address._id },
-        {
-          $set: {
-            "address.$": payload.address,
+  try {
+    const { address } = req.body
+    if (address) {
+      if (address._id) {
+        UserAddress.findOneAndUpdate(
+          { user: req.id, "address._id": address._id },
+          {
+            $set: {
+              "address.$": address,
+            },
+          }
+        ).exec((error, address) => {
+          if (error) return res.status(400).json({ error })
+          if (address) {
+            res.status(201).json({ address })
+          }
+        })
+      } else {
+        UserAddress.findOneAndUpdate(
+          { user: req.id },
+          {
+            $push: {
+              address: address,
+            },
           },
-        }
-      ).exec((error, address) => {
-        if (error) return res.status(400).json({ error });
-        if (address) {
-          res.status(201).json({ address });
-        }
-      });
+          { new: true, upsert: true }
+        ).exec((error, address) => {
+          if (error) return res.status(400).json({ error })
+          if (address) {
+            res.status(201).json({ address })
+          }
+        })
+      }
     } else {
-      UserAddress.findOneAndUpdate(
-        { user: req.id },
-        {
-          $push: {
-            address: payload.address,
-          },
-        },
-        { new: true, upsert: true }
-      ).exec((error, address) => {
-        if (error) return res.status(400).json({ error });
-        if (address) {
-          res.status(201).json({ address });
-        }
-      });
+      res.status(400).json({ error: "Params address required" })
     }
-  } else {
-    res.status(400).json({ error: "Params address required" });
+  } catch (error) {
+    console.log(error)
   }
-};
+}
 
 exports.getAddress = (req, res) => {
   UserAddress.findOne({ user: req.id }).exec((error, userAddress) => {
-    if (error) return res.status(400).json({ error });
+    if (error) return res.status(400).json({ error })
     if (userAddress) {
-      res.status(200).json({ userAddress });
+      res.status(200).json({ userAddress })
     }
-  });
-};
+  })
+}
