5e58213bd58cd2898f30982d1c0e5b5516afae4a
Raython <rasheed998855@gmail.com>
2023-04-07 08:14:41 +0300

    ss

--- backend/src/routes/address.js
+++ backend/src/routes/address.js
@@ -4,6 +4,6 @@ const router = express.Router()
 const verifyJwt = require("../middleware/VerifyJwt")
 
 router.post("/user/address/create", verifyJwt, addAddress)
-router.post("/user/getaddress", getAddress)
+router.post("/user/getaddress", verifyJwt, getAddress)
 
 module.exports = router
