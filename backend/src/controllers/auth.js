d496bf752c684d784803870317294d28cc72949e
Raython <rasheed998855@gmail.com>
2023-06-03 08:57:10 +0300

    ss

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -94,7 +94,7 @@ module.exports.postSignIn = async (req, res, next) => {
       secure: true,
       maxAge: 7 * 24 * 60 * 60 * 60 * 60 * 60 * 1000,
     });
-    res.status(201).json(accessToken);
+    res.status(201).json({ accessToken, email });
   } catch (err) {
     console.log(err);
   }
